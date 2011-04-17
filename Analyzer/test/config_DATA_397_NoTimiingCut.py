7f61f89324498805daf2c9098399b1bd8a22e83b
schauhan <>
2011-04-17 22:35:40 +0000

    added pixel seedinng to rereco

--- Analyzer/test/config_DATA_397_NoTimiingCut.py
+++ Analyzer/test/config_DATA_397_NoTimiingCut.py
@@ -6,7 +6,7 @@ from PhysicsTools.PatAlgos.tools.coreTools import *
 #---Needed to Reconsctruct on the fly from uncleaned SCs without timing cut for slpikes
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-process.load('RecoEgamma.EgammaPhotonProducers.conversionTracks_cff')
+#process.load('RecoEgamma.EgammaPhotonProducers.conversionTracks_cff')
 
 process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
@@ -32,32 +32,36 @@ addTcMET(process,"TC")
 # switch on PAT trigger
 from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
 
-# Select jets
+# Select calo jets
+process.patJetCorrFactors.levels = cms.vstring(['L2Relative','L3Absolute','L5Flavor', 'L7Parton','L2L3Residual'])
 process.selectedPatJets.cut = cms.string('pt > 10 & abs(eta) < 3.0')
+
+
+# Add PF jets
+from PhysicsTools.PatAlgos.tools.jetTools import *
+addJetCollection(process,cms.InputTag('ak5PFJets'),
+                 'AK5', 'PF',
+                 doJTA        = True,
+                 doBTagging   = True,
+                 jetCorrLabel = ('AK5PF', cms.vstring(['L2Relative', 'L3Absolute','L5Flavor','L7Parton','L2L3Residual'])),
+                 doType1MET    = True,
+                 doL1Cleaning  = True,
+                 doL1Counters  = False,
+                 genJetCollection=cms.InputTag("ak5GenJets"),
+                 doJetID       = True,
+                 jetIdLabel    = "ak5"
+                )
+process.selectedPatJetsAK5PF.cut = cms.string('pt > 10')
+
 
 # Add the files 
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 
 readFiles.extend( [
-#'dcache:/pnfs/cms/WAX/11/store/user/lpcgg/miceli/Photon/PhotonSkim_Run2010B-Nov4ReReco_v1_146240-147829/3c009990a403ffefbef1d67824f3333a/phoskim_88_2_Faz.root'
         '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E8139119-2110-E011-855E-003048C69318.root',
         '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E80A4E9E-1F10-E011-B243-003048D439A8.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E6F8E442-2010-E011-B25D-0030487D5EB1.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E6D83A9A-1F10-E011-9F55-0030487D5EB1.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E4698D2B-2010-E011-89CF-003048C693C8.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E299636E-1F10-E011-A71F-0030487D5D7B.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E25FD4C8-1F10-E011-98DA-0030487E4EC5.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E05C8E7D-1F10-E011-9B13-0030487F1665.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E0088F3F-1F10-E011-85E6-003048C693BA.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/DE243F7E-2010-E011-8A58-003048C6903C.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/DC8D550F-2110-E011-80B0-003048C69292.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/DC20AA28-2010-E011-B7BD-0030487D5EB1.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/DAE6744F-1F10-E011-A3F6-0030487D5DA5.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/DA222625-2010-E011-A7DC-003048C69318.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/D8DB462E-2110-E011-A12E-003048C69318.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/D8BBDFC5-1F10-E011-80A1-003048C693C8.root',
-        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/D8484310-2110-E011-8AD5-003048C693D0.root'
+        '/store/data/Run2010A/EG/RECO/Dec22ReReco_v1/0027/E6F8E442-2010-E011-B25D-0030487D5EB1.root'
 
     ] );
 
@@ -77,6 +81,7 @@ process.demo = cms.EDAnalyzer('Analyzer',
                               muonTag          = cms.untracked.InputTag("selectedPatMuons"),
                               cosMuonTag       = cms.untracked.InputTag("muonsFromCosmics"),
                               jetTag           = cms.untracked.InputTag("selectedPatJets"),
+                              pfjetTag         = cms.untracked.InputTag("selectedPatJetsAK5PF"),
                               photonTag        = cms.untracked.InputTag("selectedPatPhotons"),
                               cscTag           = cms.untracked.InputTag("cscSegments"),
                               rpcTag           = cms.untracked.InputTag("rpcRecHits"),
@@ -88,6 +93,7 @@ process.demo = cms.EDAnalyzer('Analyzer',
                               HLTriggerResults = cms.untracked.InputTag("TriggerResults","","HLT"),
                               Tracks           = cms.untracked.InputTag("generalTracks"),
                               Vertices         = cms.untracked.InputTag("offlinePrimaryVertices","",""),
+                              BeamHaloSummary  = cms.untracked.InputTag("BeamHaloSummary"),
                               outFile          = cms.untracked.string("Histo_Data_A.root"),
                               runphotons       = cms.untracked.bool(True),
                               runHErechit      = cms.untracked.bool(True),
@@ -97,6 +103,7 @@ process.demo = cms.EDAnalyzer('Analyzer',
                               runPFmet         = cms.untracked.bool(True),
                               runTCmet         = cms.untracked.bool(True),
                               runjets          = cms.untracked.bool(True),
+                              runpfjets        = cms.untracked.bool(True),
                               runelectrons     = cms.untracked.bool(True),
                               runtaus          = cms.untracked.bool(True),
                               runmuons         = cms.untracked.bool(True),
@@ -108,6 +115,7 @@ process.demo = cms.EDAnalyzer('Analyzer',
                               runtracks        = cms.untracked.bool(True),
                               runvertex        = cms.untracked.bool(True),
                               runCSCseg        = cms.untracked.bool(True),
+                              runBeamHaloSummary= cms.untracked.bool(True),
                               runRPChit        = cms.untracked.bool(True),
                               debug            = cms.untracked.bool(False)
                               )
@@ -116,7 +124,7 @@ process.demo = cms.EDAnalyzer('Analyzer',
 
 
 process.monoSkim = cms.EDFilter("MonoPhotonSkimmer",
-  phoTag = cms.InputTag("photons"),   # photons::RECO will look for original collection
+  phoTag = cms.InputTag("photons"),    #"photons::RECO" will look for original collection
   selectEE = cms.bool(True),
   selectTrack = cms.bool(False),
   ecalisoOffsetEB = cms.double(5000.),
@@ -135,6 +143,7 @@ process.monoSkim = cms.EDFilter("MonoPhotonSkimmer",
   scHighEtThreshEE = cms.double(100.),
  )
 
+
 #Remove the time severity flag from cleanedHybridSuperCluster
 process.cleanedHybridSuperClusters.RecHitSeverityToBeExcluded= cms.vint32(4,5)
 
@@ -142,13 +151,60 @@ process.cleanedHybridSuperClusters.RecHitSeverityToBeExcluded= cms.vint32(4,5)
 process.uncleanedHybridSuperClusters.RecHitSeverityToBeExcluded= cms.vint32(999)
 
 
-#Reconstrucition of photon from SC without the timing cuts
-process.photonReReco = cms.Sequence(
-        process.ecalClusters*
-	ckfTracksFromConversions*
-	process.conversionSequence*
-	process.photonSequence*
-	process.photonIDSequence)
+
+#----------------From OLD config-------------
+process.newSeedFromTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters  =20000
+process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters        =2000
+process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters        =20000
+process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters     =20000
+process.thTripletsA.ClusterCheckPSet.MaxNumberOfPixelClusters        =5000
+process.thTripletsB.ClusterCheckPSet.MaxNumberOfPixelClusters        =5000
+ 
+ 
+#Local reco
+process.cleanedLocalreco = cms.Sequence(siPixelRecHits*
+                                        ckftracks*
+                                        particleFlowCluster)
+
+#Seeding electorn
+process.cleanedElectronseeding = cms.Sequence(trackerDrivenElectronSeeds*
+                                              ecalDrivenElectronSeeds*
+                                              electronMergedSeeds)
+#tracking
+process.cleanedElectrontracking = cms.Sequence(electronCkfTrackCandidates*
+                                               electronGsfTracks)
+#gsf electorn sequence
+process.cleanedElectronreco = cms.Sequence(particleFlowReco*
+                                           pfElectronTranslator*
+                                           gsfElectronSequence)
+
+#just make things simpler below
+process.NewconversionTrackSequence = cms.Sequence( process.conversionTrackSequence+
+                                                   process.trackerOnlyConversionSequence)
+
+
+#process.conversionTrackSequence = cms.Sequence(process.ckfTracksFromConversions+process.conversionTrackProducers+process.conversionTrackMergers)
+
+
+#egammaSkimReco
+process.cleanedEgammaSkimReco = cms.Sequence(process.cleanedLocalreco*
+                                             process.cleanedElectronseeding*
+                                             process.cleanedElectrontracking*
+                                             process.cleanedElectronreco*
+                                             process.NewconversionTrackSequence* 
+                                             process.conversionSequence*
+                                             process.photonSequence*
+                                             process.interestingEgammaIsoDetIds*
+                                             process.photonIDSequence*
+                                             process.eIdSequence
+                                             )
+##---------------------------------------------------------------------------
+
+#Reconstrucition of electron/photon from SC without the timing cuts
+process.photonReReco = cms.Sequence( process.ecalClusters*
+                                     process.cleanedEgammaSkimReco       
+                                   )
 
 #All paths are here
 process.p = cms.Path(
@@ -160,14 +216,15 @@ process.p = cms.Path(
 
 
 # reduce verbosity
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
-
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(2000)
 # process all the events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 #process.options.wantSummary = True
 
 
 process.schedule=cms.Schedule(process.p)
+
+
 try:
    import readline
 except ImportError:
@@ -176,3 +233,4 @@ else:
    import rlcompleter
    readline.parse_and_bind("tab: complete")
 
+#print process.dumpPython()
